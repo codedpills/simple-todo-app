b2efeb2322b1983759f4c87083dd20d30c9f11d9
codedpills <codedpills@gmail.com>
2019-05-13 14:38:33 +0000

    added initial state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'bought some milk' },
+      { id: 2, content: 'played mario cart' }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
 }
 
 export default App;
