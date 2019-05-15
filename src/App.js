81f48adaa5592d30f5657ef09cb2c39b2468bfc5
codedpills <codedpills@gmail.com>
2019-05-15 16:06:11 +0000

    bootstraps app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {Component} from "react";
+import Todos from './Todos';
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
@@ -7,10 +9,27 @@ class App extends Component {
       { id: 2, content: 'played mario cart' }
     ]
   };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
-      <div className="App">
-        
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
